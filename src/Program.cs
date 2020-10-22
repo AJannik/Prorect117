007e6c5810fc7286342b7b741c64f3ff423592cd
AJannik <jannik.schmidberger@gmail.com>
2020-10-22 13:41:06 +0200

    Added StyleCop and formatted files to ruleset
    StyleCop rules can be changed in the .editorconfig

--- src/Program.cs
+++ src/Program.cs
@@ -1,33 +1,33 @@
-﻿using Game;
+﻿using System;
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
-using System;
 
 namespace Example
 {
-	internal class Program
-	{
-		[STAThread]
-		private static void Main()
-		{
-			var window = new GameWindow(512, 512);
+    internal class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            var window = new GameWindow(512, 512);
 
-			void Draw()
-			{
-				//clear screen - what happens without?
-				GL.Clear(ClearBufferMask.ColorBufferBit);
-				//draw a quad
-				GL.Begin(PrimitiveType.Quads);
-				GL.Vertex2(0.0f, 0.0f); //draw first quad corner
-				GL.Vertex2(0.5f, 0.0f);
-				GL.Vertex2(0.5f, 0.5f);
-				GL.Vertex2(0.0f, 0.5f);
-				GL.End();
-				window.SwapBuffers(); // buffer swap needed for double buffering
-			}
+            void Draw()
+            {
+                // clear screen - what happens without?
+                GL.Clear(ClearBufferMask.ColorBufferBit);
 
-			window.RenderFrame += (s, a) => Draw();
-			window.Run();
-		}
-	}
+                // draw a quad
+                GL.Begin(PrimitiveType.Quads);
+                GL.Vertex2(0.0f, 0.0f); // draw first quad corner
+                GL.Vertex2(0.5f, 0.0f);
+                GL.Vertex2(0.5f, 0.5f);
+                GL.Vertex2(0.0f, 0.5f);
+                GL.End();
+                window.SwapBuffers(); // buffer swap needed for double buffering
+            }
+
+            window.RenderFrame += (s, a) => Draw();
+            window.Run();
+        }
+    }
 }
