033c69109ff6fdec8d18424451972569c4d5b705
AJannik <jannik.schmidberger@gmail.com>
2020-10-22 13:21:55 +0200

    Initial project set up

---
+++ src/Temp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game
+{
+    public class Temp
+    {
+        public int Add2int(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
