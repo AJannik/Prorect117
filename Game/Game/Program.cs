0cfd0a2f0e27111a8050925bc2f7fbfd02c07d4d
AJannik <jannik.schmidberger@gmail.com>
2020-10-28 18:24:44 +0100

    Project setup for netcore 3.1

---
+++ Game/Game/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace Game
+{
+    internal class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            var window = new GameWindow(512, 512);
+            void Draw()
+            {
+                // clear screen - what happens without?
+                GL.Clear(ClearBufferMask.ColorBufferBit);
+
+                // draw a quad
+                GL.Begin(PrimitiveType.Quads);
+                GL.Vertex2(0.0f, 0.0f); // draw first quad corner
+                GL.Vertex2(0.5f, 0.0f);
+                GL.Vertex2(0.5f, 0.5f);
+                GL.Vertex2(0.0f, 0.5f);
+                GL.End();
+                window.SwapBuffers(); // buffer swap needed for double buffering
+            }
+
+            window.RenderFrame += (s, a) => Draw();
+            window.Run();
+        }
+    }
+}
---
+++ Game/Game/Temp.cs
@@ -0,0 +1,10 @@
+﻿namespace Game
+{
+    public class Temp
+    {
+        public int Add2ints(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
