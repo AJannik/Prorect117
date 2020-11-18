c4b9cd5b1aa47a615723b875dfaca8ec38d46b9d
AJannik <jannik.schmidberger@gmail.com>
2020-11-18 20:11:19 +0100

    Updated Position to return world coordinates while storing local coordinates

--- Game/Game/Components/CTransform.cs
+++ Game/Game/Components/CTransform.cs
@@ -6,12 +6,30 @@ namespace Game.Component
 {
     public class CTransform : IComponent
     {
+        private Vector2 pos = new Vector2(0f, 0f); // local coordinates
+
         public CTransform()
         {
             Position = new Vector2(0, 0);
         }
 
-        public Vector2 Position { get; set; }
+        public Vector2 Position // world coordinates
+        {
+            get
+            {
+                if (MyGameObject.Parent != null)
+                {
+                    return pos + MyGameObject.Parent.transform.Position;
+                }
+
+                return pos;
+            }
+
+            set
+            {
+                pos = value;
+            }
+        }
 
         public GameObject MyGameObject { get; set; }
 
