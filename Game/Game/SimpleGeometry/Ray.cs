59fdd891df2b152b3d392b25a0a84684fbd7587c
AJannik <jannik.schmidberger@gmail.com>
2020-12-06 14:48:13 +0100

    Added Circle and Ray as simple Geometry

---
+++ Game/Game/SimpleGeometry/Ray.cs
@@ -0,0 +1,26 @@
+﻿using OpenTK;
+
+namespace Game.SimpleGeometry
+{
+    public class Ray
+    {
+        private Vector2 direction = new Vector2(0f, 0f);
+
+        public Ray(Vector2 startPos, Vector2 direction, float length)
+        {
+            StartPos = startPos;
+            Direction = direction;
+            Length = length;
+        }
+
+        public Vector2 StartPos { get; set; }
+
+        public Vector2 Direction
+        {
+            get { return direction; }
+            set { direction = value.Normalized(); }
+        }
+
+        public float Length { get; set; }
+    }
+}
