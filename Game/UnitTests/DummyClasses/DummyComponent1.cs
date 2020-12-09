6994a3a25f3fee197b3d04dc67f0f3a06f9fd03d
AJannik <jannik.schmidberger@gmail.com>
2020-12-09 18:55:22 +0100

    Changed TestsGameObject to use dummy components

---
+++ Game/UnitTests/DummyClasses/DummyComponent1.cs
@@ -0,0 +1,16 @@
+﻿using Game;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.DummyClasses
+{
+    public class DummyComponent1 : IComponent
+    {
+        public GameObject MyGameObject { get; set; } = null;
+
+        public void Update(float deltaTime)
+        {            
+        }
+    }
+}
---
+++ Game/UnitTests/DummyClasses/DummyComponent2.cs
@@ -0,0 +1,17 @@
+﻿using Game;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.DummyClasses
+{
+    public class DummyComponent2 : IComponent
+    {
+        public GameObject MyGameObject { get; set; } = null;
+
+        public void Update(float deltaTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
