1d1b1b8a76ff190d6b5a277430407b3fc9eb2902
Tobias Weigelt <tobias@weigelt-frickingen.de>
2020-12-18 16:08:56 +0100

    Added Basic PowerDownComponent

---
+++ Game/Game/Components/CPowerDownScript.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Game.Interfaces;
+
+namespace Game.Components
+{
+    public class CPowerDownScript : IComponent
+    {
+        public CPowerDownScript()
+        {
+
+        }
+
+        public GameObject MyGameObject { get; set; } = null;
+
+        public CCircleCollider Collider { get; set; }
+
+        public void Update(float deltaTime)
+        {
+            // if (Collider.TriggerEntered)
+            {
+                // Apply Effect
+            }
+        }
+    }
+}
