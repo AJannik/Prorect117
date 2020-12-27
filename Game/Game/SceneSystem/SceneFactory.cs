6bdd27cd72c657846d4a9bd585bc069d2fd9c546
AJannik <jannik.schmidberger@gmail.com>
2020-12-27 21:34:18 +0100

    Removed unused Scenes

--- Game/Game/SceneSystem/SceneFactory.cs
+++ Game/Game/SceneSystem/SceneFactory.cs
@@ -7,7 +7,7 @@ namespace Game.SceneSystem
 {
     internal class SceneFactory
     {
-        internal int NumScenes { get; } = 4;
+        internal int NumScenes { get; } = 1;
 
         public Scene BuildScene(int num)
         {
@@ -15,64 +15,9 @@ namespace Game.SceneSystem
             {
                 case 0:
                     return BuildScene0();
-                case 1:
-                    return BuildScene1();
-                case 2:
-                    return BuildScene2();
-                case 3:
-                    return BuildScene3();
                 default:
                     throw new ArgumentOutOfRangeException($"There is no Scene numbered {num}!");
             }
-        }
-
-        private Scene BuildScene3()
-        {
-            Scene scene = new Scene();
-
-            GameObject camera = ObjectFactory.BuildCamera(scene, Vector2.Zero);
-            ObjectFactory.BuildPlatform(scene, new Vector2(0f, -4f), 4);
-            GameObject ball = ObjectFactory.BuildBall(scene, Vector2.Zero);
-
-            camera.SetParent(ball);
-            camera.GetComponent<CCamera>().Scale = 2f;
-
-            return scene;
-        }
-
-        private Scene BuildScene1()
-        {
-            Scene scene = new Scene();
-
-            GameObject camera = ObjectFactory.BuildCamera(scene, Vector2.Zero);
-            GameObject parentQuad = ObjectFactory.BuildSprite(scene, Vector2.Zero);
-            GameObject childQuad = ObjectFactory.BuildSprite(scene, new Vector2(3f, 0f));
-            GameObject childChildQuad = ObjectFactory.BuildSprite(scene, new Vector2(1.5f, 0f));
-
-            camera.GetComponent<CCamera>().Scale = 5f;
-            childQuad.SetParent(parentQuad);
-            childChildQuad.SetParent(childQuad);
-            childQuad.Transform.Scale = new Vector2(0.8f, 0.8f);
-            childChildQuad.Transform.Scale = new Vector2(0.8f, 0.8f);
-
-            return scene;
-        }
-
-        private Scene BuildScene2()
-        {
-            Scene scene = new Scene();
-
-            GameObject camera = ObjectFactory.BuildCamera(scene, Vector2.Zero);
-            ObjectFactory.BuildPlatform(scene, new Vector2(0f, -4f), 4);
-            ObjectFactory.BuildPlatform(scene, new Vector2(5f, -4f), 4);
-            ObjectFactory.BuildPlatform(scene, new Vector2(0f, -6f), 32);
-
-            GameObject player = ObjectFactory.BuildPlayer(scene, new Vector2(0f, 3.0f));
-            camera.SetParent(player);
-
-            camera.GetComponent<CCamera>().Scale = 3f;
-
-            return scene;
         }
 
         private Scene BuildScene0()
