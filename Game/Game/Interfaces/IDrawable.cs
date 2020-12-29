9534799a1496871cd37e5c1d900caedd4f453694
AJannik <jannik.schmidberger@gmail.com>
2020-12-29 16:20:26 +0100

    Added new Interfaces

--- Game/Game/Interfaces/IDrawable.cs
+++ Game/Game/Interfaces/IDrawable.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Game.Interfaces
+﻿namespace Game.Interfaces
 {
     public interface IDrawable
     {
+        public int Layer { get; set; }
+
         public void Draw();
     }
 }
---
+++ Game/Game/Interfaces/IResizeable.cs
@@ -0,0 +1,7 @@
+﻿namespace Game.Interfaces
+{
+    public interface IResizeable
+    {
+        public void Resize(int width, int height);
+    }
+}
