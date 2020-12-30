69c114851f94d899c68e8ae432ec7259bd05fc04
AJannik <jannik.schmidberger@gmail.com>
2020-12-30 16:39:39 +0100

    Added CCanvas and Texture for coins

--- Game/Game/Components/UI/CCanvas.cs
+++ Game/Game/Components/UI/CCanvas.cs
@@ -1,10 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Game.Interfaces;
+using Game.Tools;
+using OpenTK;
 
 namespace Game.Components.UI
 {
-    public class CCanvas
+    public class CCanvas : IComponent, IUpdateable
     {
+        public GameObject MyGameObject { get; set; }
+
+        public Matrix4 CanvasMatrix { get; set; } = Matrix4.Identity;
+
+        public void Update(float deltaTime)
+        {
+            UpdateMatrix();
+        }
+
+        private void UpdateMatrix()
+        {
+            Matrix4 aspect = Transformation.Scale(16f / 9f, 1f);
+
+            Matrix4 canvasScale = Transformation.Scale(MyGameObject.GetComponent<CCamera>().Scale);
+
+            CanvasMatrix = Transformation.Combine(canvasScale, aspect);
+        }
     }
 }
