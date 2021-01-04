35fc05bcc9c16f0fc7bd332e710e958ef77b0754
Fi0x <felpech@aol.com>
2021-01-04 19:24:31 +0100

    Corrected player HP

--- Game/Game/SceneSystem/GameManager.cs
+++ Game/Game/SceneSystem/GameManager.cs
@@ -11,7 +11,7 @@ namespace Game.SceneSystem
         // Stay at level-change
         public int Coins { get; set; }
 
-        public float PlayerHealth { get; set; } = 9f;
+        public float PlayerHealth { get; set; } = 100f;
 
         // If the player has collected a key in this level
         public bool Key { get; set; } = false;
