83f9159816765294878025ca4140a7ffc475193c
ld-183106 <lukas.daibert@rwu.de>
2021-01-05 03:15:33 +0100

    added missing file

---
+++ Game/Game/Components/CPeriodicMovement.cs
@@ -0,0 +1,90 @@
+using Game.Interfaces;
+using OpenTK;
+
+namespace Game.Components
+{
+    public class CPeriodicMovement : IUpdateable, IComponent
+    {
+        private Vector2 start;
+        private Vector2 end;
+        private float moveSpeed;
+
+        private bool Forward { get; set; } = true;
+
+        private Vector2 MoveVector { set; get; }
+
+        public GameObject MyGameObject { get; set; }
+
+        public Vector2 Start
+        {
+            get
+            {
+                return this.start;
+            }
+
+            set
+            {
+                this.start = value;
+                UpdateMoveVector();
+            }
+        }
+
+        public Vector2 End
+        {
+            get
+            {
+                return end;
+            }
+
+            set
+            {
+                end = value;
+                UpdateMoveVector();
+            }
+        }
+
+        public float MoveSpeed
+        {
+            get
+            {
+                return moveSpeed;
+            }
+
+            set
+            {
+                moveSpeed = value;
+                UpdateMoveVector();
+            }
+        }
+
+        public void Update(float deltaTime)
+        {
+            Vector2 movement = MoveVector * deltaTime;
+            MyGameObject.Transform.Position = MyGameObject.Transform.Position + movement;
+
+            float x = MyGameObject.Transform.Position.X;
+            float y = MyGameObject.Transform.Position.Y;
+
+            if ((x > End.X && x > Start.X) || (x < End.X && x < Start.X) || (y > End.Y && y > Start.Y) || (y < End.Y && y < Start.Y))
+            {
+                if (Forward)
+                {
+                    MyGameObject.Transform.Position = Start;
+                    MoveVector = MoveVector * -1;
+                    Forward = !Forward;
+                }
+                else
+                {
+                    MyGameObject.Transform.Position = End;
+                    MoveVector = MoveVector * -1;
+                    Forward = !Forward;
+                }
+            }
+        }
+
+        private void UpdateMoveVector()
+        {
+            MoveVector = Vector2.Normalize(Vector2.Subtract(Start, End)) * MoveSpeed;
+        }
+    }
+}
