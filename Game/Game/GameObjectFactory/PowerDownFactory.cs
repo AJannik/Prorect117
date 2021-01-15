2baab57338873a6f4feb5b712095e8b78938d97c
Fi0x <felpech@aol.com>
2021-01-15 12:08:52 +0100

    Added remaining power-downs to PowerDownFactory.cs

--- Game/Game/GameObjectFactory/PowerDownFactory.cs
+++ Game/Game/GameObjectFactory/PowerDownFactory.cs
@@ -85,5 +85,83 @@ namespace Game.GameObjectFactory
 
             return powerDown;
         }
+
+        public static GameObject NoRoll(Scene scene, Vector2 position)
+        {
+            GameObject powerDown = new GameObject(scene, "NoRoll");
+            powerDown.Transform.Position = position;
+
+            powerDown.AddComponent<CCircleTrigger>();
+            powerDown.GetComponent<CCircleTrigger>().Geometry.Size = new Vector2(1, 1);
+
+            powerDown.AddComponent<CPowerDownScript>();
+            CPowerDownScript pdScript = powerDown.GetComponent<CPowerDownScript>();
+            pdScript.Effect = Tools.EffectType.Silenced;
+            pdScript.SetupTrigger(powerDown.GetComponent<CCircleTrigger>());
+
+            powerDown.AddComponent<CRender>();
+            CRender render = powerDown.GetComponent<CRender>();
+            render.LoadAndSetTexture("Content.PowerDownBlue.png");
+            render.Layer = 30;
+            render.SetSize(1f, 1f);
+
+            powerDown.AddComponent<CParticleSystem>();
+            CParticleSystem particleSystem = powerDown.GetComponent<CParticleSystem>();
+            particleSystem.SystemColor = Color.White;
+            particleSystem.FadeColor = Color.GreenYellow;
+            particleSystem.FadeOut = true;
+            particleSystem.FadesIntoColor = true;
+            particleSystem.MaxParticles = 40;
+            particleSystem.ParticleSpawnRate = 30;
+            particleSystem.UseForceField = false;
+            particleSystem.Actice = true;
+            particleSystem.DirectionRandomness = 10f;
+            particleSystem.MaxParticleLifetime = 1.5f;
+            particleSystem.SizeRandomness = 0.5f;
+            particleSystem.SizeMode = ParticleSizeMode.Shrinking;
+            particleSystem.SizeChangeSpeed = 0.01f;
+            particleSystem.ParticleSize = 0.4f;
+
+            return powerDown;
+        }
+
+        public static GameObject Weakness(Scene scene, Vector2 position)
+        {
+            GameObject powerDown = new GameObject(scene, "Weakness");
+            powerDown.Transform.Position = position;
+
+            powerDown.AddComponent<CCircleTrigger>();
+            powerDown.GetComponent<CCircleTrigger>().Geometry.Size = new Vector2(1, 1);
+
+            powerDown.AddComponent<CPowerDownScript>();
+            CPowerDownScript pdScript = powerDown.GetComponent<CPowerDownScript>();
+            pdScript.Effect = Tools.EffectType.Weakness;
+            pdScript.SetupTrigger(powerDown.GetComponent<CCircleTrigger>());
+
+            powerDown.AddComponent<CRender>();
+            CRender render = powerDown.GetComponent<CRender>();
+            render.LoadAndSetTexture("Content.PowerDownYellow.png");
+            render.Layer = 30;
+            render.SetSize(1f, 1f);
+
+            powerDown.AddComponent<CParticleSystem>();
+            CParticleSystem particleSystem = powerDown.GetComponent<CParticleSystem>();
+            particleSystem.SystemColor = Color.White;
+            particleSystem.FadeColor = Color.GreenYellow;
+            particleSystem.FadeOut = true;
+            particleSystem.FadesIntoColor = true;
+            particleSystem.MaxParticles = 40;
+            particleSystem.ParticleSpawnRate = 30;
+            particleSystem.UseForceField = false;
+            particleSystem.Actice = true;
+            particleSystem.DirectionRandomness = 10f;
+            particleSystem.MaxParticleLifetime = 1.5f;
+            particleSystem.SizeRandomness = 0.5f;
+            particleSystem.SizeMode = ParticleSizeMode.Shrinking;
+            particleSystem.SizeChangeSpeed = 0.01f;
+            particleSystem.ParticleSize = 0.4f;
+
+            return powerDown;
+        }
     }
 }
