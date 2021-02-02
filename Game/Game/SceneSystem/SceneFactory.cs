3b7b0e020a5e1887a87fd366b6684f448461ed2f
ld-183106 <lukas.daibert@rwu.de>
2021-02-02 19:32:11 +0100

    Added rock decorations in level 3

--- Game/Game/SceneSystem/SceneFactory.cs
+++ Game/Game/SceneSystem/SceneFactory.cs
@@ -722,10 +722,26 @@ namespace Game.SceneSystem
             ObjectFactory.BuildSprite(scene, new Vector2(25f, -58f), new Vector2(4, 4), "Environment.TreeSmall.png");
             ObjectFactory.BuildSprite(scene, new Vector2(20f, -29.5f), new Vector2(3, 3), "Environment.TreeSmall.png");
             ObjectFactory.BuildSprite(scene, new Vector2(14f, -2.5f), new Vector2(3, 3), "Environment.TreeSmall.png");
+            ObjectFactory.BuildSprite(scene, new Vector2(2, -6), new Vector2(2, 2), "Environment.Rock.png");
+            ObjectFactory.BuildSprite(scene, new Vector2(16, -11), new Vector2(2, 2), "Environment.Rock.png");
+            ObjectFactory.BuildSprite(scene, new Vector2(5, -15), new Vector2(2, 2), "Environment.Rock.png");
+            ObjectFactory.BuildSprite(scene, new Vector2(22, -15), new Vector2(2, 2), "Environment.Rock.png");
+            ObjectFactory.BuildSprite(scene, new Vector2(10, -22), new Vector2(2, 2), "Environment.Rock.png");
+            ObjectFactory.BuildSprite(scene, new Vector2(25, -23), new Vector2(2, 2), "Environment.Rock.png");
+            ObjectFactory.BuildSprite(scene, new Vector2(4, -31), new Vector2(2, 2), "Environment.Rock.png");
+            ObjectFactory.BuildSprite(scene, new Vector2(16, -33), new Vector2(2, 2), "Environment.Rock.png");
+            ObjectFactory.BuildSprite(scene, new Vector2(22, -39), new Vector2(2, 2), "Environment.Rock.png");
+            ObjectFactory.BuildSprite(scene, new Vector2(10, -42), new Vector2(2, 2), "Environment.Rock.png");
+            ObjectFactory.BuildSprite(scene, new Vector2(28, -49), new Vector2(2, 2), "Environment.Rock.png");
+            ObjectFactory.BuildSprite(scene, new Vector2(10, -56), new Vector2(2, 2), "Environment.Rock.png");
+            ObjectFactory.BuildSprite(scene, new Vector2(13, -59), new Vector2(2, 2), "Environment.Rock.png");
+            ObjectFactory.BuildSprite(scene, new Vector2(22, -59), new Vector2(2, 2), "Environment.Rock.png");
+            ObjectFactory.BuildSprite(scene, new Vector2(18, -62), new Vector2(2, 2), "Environment.Rock.png");
+            ObjectFactory.BuildSprite(scene, new Vector2(22, -70), new Vector2(2, 2), "Environment.Rock.png");
+            ObjectFactory.BuildSprite(scene, new Vector2(12, -77), new Vector2(2, 2), "Environment.Rock.png");
 
             // Player, exit and camera
-            // GameObject player = ObjectFactory.BuildPlayer(scene, new Vector2(27.5f, -76.9f));
-            GameObject player = ObjectFactory.BuildPlayer(scene, new Vector2(27.5f, -40.9f));
+            GameObject player = ObjectFactory.BuildPlayer(scene, new Vector2(27.5f, -76.9f));
             GameObject levelEnd = StaticRigidbodyFactory.BuildLevelEnd(scene, new Vector2(7.5f, -2.5f), new Vector2(3, 3));
             GameObject camera = ObjectFactory.BuildCamera(scene, Vector2.Zero);
             camera.SetParent(player);
